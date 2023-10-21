171799da12c6329939e05b22731b084f0985f126
Luis Felipe Copetti Rossi <rossi.luis@edu.pucrs.br>
2023-10-21 00:40:48 -0300

    adicionando search-delete-found

--- Paralelismo/src/Database.java
+++ Paralelismo/src/Database.java
@@ -3,65 +3,110 @@ import java.util.List;
 import java.util.concurrent.Semaphore;
 
 class Database {
-    private final List<Integer> data;
-    private final Semaphore semaphore;
+    private final List<Integer> lista;
+    private final Semaphore semaforo;
 
     public Database() {
-        data = new ArrayList<>();
-        semaphore = new Semaphore(1);
+        lista = new ArrayList<>();
+        semaforo = new Semaphore(1);
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Database database = new Database();
+        Database lista = new Database();
 
-       database.insert(0);
-       database.search(0);
-       database.delete(0);
+        Thread inserir1 = new Thread(() -> {
+            try {
+                lista.inserir(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread inserir2 = new Thread(() -> {
+            try {
+                lista.inserir(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread procura1 = new Thread(() -> {
+            try {
+                lista.procurar(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread procura2 = new Thread(() -> {
+            try {
+                lista.procurar(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread deleta1 = new Thread(() -> {
+            try {
+                lista.deletar(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        
+        
+        deleta1.start();
+        inserir1.start();
+        inserir2.start();
+        procura1.start();
+        procura2.start();
+        
     }
 
-    public void insert(int value) throws InterruptedException {
-        semaphore.acquire();
+    public void inserir(int valor) throws InterruptedException {
+        semaforo.acquire();
         try {
-            data.add(value);
-            System.out.println("Inserted: " + value);
+            lista.add(valor);
+            System.out.println("Inserted: " + valor);
         }catch (Exception e){
             System.out.println("Error: " + e);
         } finally {
-            semaphore.release();
+            semaforo.release();
         }
     }
 
-    public boolean search(int value) throws InterruptedException {
-        semaphore.acquire();
+    public boolean procurar(int valor) throws InterruptedException {
+        semaforo.acquire();
         try {
-            for (int item : data) {
-                if (item == value) {
-                    System.out.println("Found: " + value);
+            for (int item : lista) {
+                if (item == valor) {
+                    System.out.println("Found: " + valor);
                     return true;
                 }
             }
-            System.out.println("Not Found: " + value);
+            System.out.println("Not Found: " + valor);
             return false;
         }catch (Exception e){
             System.out.println("Error: " + e);
             return false;
         } finally {
-            semaphore.release();
+            semaforo.release();
         }
     }
 
-    public void delete(int value) throws InterruptedException {
-        semaphore.acquire();
+    public void deletar(int valor) throws InterruptedException {
+        semaforo.acquire();
         try {
-            if (data.remove(Integer.valueOf(value))) {
-                System.out.println("Deleted: " + value);
+            if (lista.remove(Integer.valueOf(valor))) {
+                System.out.println("Deleted: " + valor);
             } else {
-                System.out.println("Not Found: " + value);
+                System.out.println("Not Found: " + valor);
             }
         }catch (Exception e){
             System.out.println("Error: " + e);
         } finally {
-            semaphore.release();
+            semaforo.release();
         }
     }
 }
